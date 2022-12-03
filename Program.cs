fe29f33158eeef39c749909150cbec888a5a8f35
Dmitryi <vdg2003@rambler.ru>
2022-12-03 20:44:51 +0500

    написал код формирования массива из сторок до 3 символов

--- Program.cs
+++ Program.cs
@@ -4,3 +4,15 @@ for (int i = 0; i < 5; i++)
     array[i] = Console.ReadLine()!;
 }
 Console.Write("[" + string.Join(", ", array) + "]");
+
+string[] resalt = new string[array.Length];
+int count = 0;
+for (int i=0; i<array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        resalt[count] = array[i];
+        count ++;
+    }
+}
+Console.WriteLine(" -> [" + string.Join(", ", resalt) + "]");
